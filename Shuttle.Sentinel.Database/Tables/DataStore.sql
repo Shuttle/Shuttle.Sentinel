2646b346daed7240f41d24d7a5a4028e307cd151
Eben <me@ebenroux.co.za>
2017-07-20 14:29:27 +0200

    - datastores

---
+++ Shuttle.Sentinel.Database/Tables/DataStore.sql
@@ -0,0 +1,11 @@
+﻿CREATE TABLE [dbo].[DataStore]
+(
+	[Id] UNIQUEIDENTIFIER NOT NULL PRIMARY KEY, 
+    [Name] VARCHAR(130) NOT NULL, 
+    [ConnectionString] VARCHAR(260) NOT NULL, 
+    [ProviderName] VARCHAR(130) NOT NULL
+)
+
+GO
+
+CREATE UNIQUE INDEX [IX_DataStore_Name] ON [dbo].[DataStore] ([Name])
