ba10057aabe4452ccfa5cf04122bc1b7da57d4a2
Eben <me@ebenroux.co.za>
2017-08-05 19:48:20 +0200

    - messages

--- Shuttle.Sentinel.Messages/v1/RegisterEndpointCommand.cs
+++ Shuttle.Sentinel.Messages/v1/RegisterEndpointCommand.cs
@@ -3,6 +3,7 @@
     public class RegisterEndpointCommand
     {
         public string EndpointName { get; set; }
+        public string EntryAssemblyQualifiedName { get; set; }
         public string MachineName { get; set; }
         public string BaseDirectory { get; set; }
         public string IPAddress { get; set; }
