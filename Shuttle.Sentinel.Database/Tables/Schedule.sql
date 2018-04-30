e86be468b618f77f85e21257d5b72cce33a3f6e0
eben-roux <me@ebenroux.co.za>
2018-04-30 19:57:38 +0200

    - scheduling

--- Shuttle.Sentinel.Database/Tables/Schedule.sql
+++ Shuttle.Sentinel.Database/Tables/Schedule.sql
@@ -1,8 +1,16 @@
 ﻿CREATE TABLE [dbo].[Schedule] (
-    [Name]              VARCHAR (120) NOT NULL,
-    [InboxWorkQueueUri] VARCHAR (130) NOT NULL,
-    [CronExpression]    VARCHAR (25)  NOT NULL,
-    [NextNotification]  DATETIME      NOT NULL,
-    CONSTRAINT [PK_Schedule] PRIMARY KEY CLUSTERED ([Name] ASC, [InboxWorkQueueUri] ASC)
+    [Id]                UNIQUEIDENTIFIER NOT NULL,
+    [Name]              VARCHAR (120)    NOT NULL,
+    [InboxWorkQueueUri] VARCHAR (130)    NOT NULL,
+    [CronExpression]    VARCHAR (25)     NOT NULL,
+    [NextNotification]  DATETIME         NOT NULL,
+    CONSTRAINT [PK_Schedule] PRIMARY KEY NONCLUSTERED ([Id] ASC)
 );
+
+
+
+
+GO
+CREATE NONCLUSTERED INDEX [IX_Schedule]
+    ON [dbo].[Schedule]([Name] ASC, [InboxWorkQueueUri] ASC, [CronExpression] ASC);
 
