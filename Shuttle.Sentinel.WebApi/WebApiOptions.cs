19d9d3ce75664f3f57f3deef187ac3f9d87ca8e6
Eben <me@ebenroux.co.za>
2022-10-10 08:31:08 +0200

    - removed data stores

--- Shuttle.Sentinel.WebApi/WebApiOptions.cs
+++ Shuttle.Sentinel.WebApi/WebApiOptions.cs
@@ -4,6 +4,8 @@ namespace Shuttle.Sentinel.WebApi;
 
 public class WebApiOptions
 {
+    public const string SectionName = "Shuttle:Sentinel:WebApi";
+
     public string SiteUrl { get; set; }
     public Type SerializerType { get; set; }
     public TimeSpan HeartbeatRecoveryDuration { get; set; }
